d8c3b8ffe1c3c569886fa9cbb145bdf79d22ccfb
seif899 <seifbchini7@gmail.com>
2023-07-07 18:35:22 +0100

    finished a basic working version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const LINE_NUMBER=1;
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+const LINE_NUMBER = urlParams.get("line");
+
+
 const currentRoute = window.location.pathname;
+
+
+
 const requestData = {
     id: currentRoute.substring(1)
 }
@@ -47,101 +56,97 @@ function start(data){
     var $fen = $('#fen')
     var $pgn = $('#pgn')
 
+
     game.load_pgn(data.rows[0].moves);
-
-    function onDragStart (source, piece, position, orientation) {
-    // do not pick up pieces if the game is over
-    if (game.game_over()) return false
-
-    // only pick up pieces for the side to move
-    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-        return false
-    }
-    }
-
-    function onDrop (source, target) {
-    // see if the move is legal
-    var move = game.move({
-        from: source,
-        to: target,
-        promotion: 'q' // NOTE: always promote to a queen for example simplicity
-    })
-
-    // illegal move
-    if (move === null) return 'snapback'
-
-    updateStatus()
+    
+    
+    stockfish.postMessage(`position fen ${game.fen()}`);
+    stockfish.postMessage("go depth 10");
+    stockfish.onmessage = (e)=>{
+        if ((e.data).startsWith("info depth 10 seldepth") && (e.data).includes(`multipv ${LINE_NUMBER}`)){
+            const index=(e.data).indexOf(" pv ");
+            const line = e.data.substring(index+4);
+            const movesList = line.split(" ");
+            startTraining(movesList);
+            
+        }
     }
     
-
-    // update the board position after the piece snap
-    // for castling, en passant, pawn promotion
-    function onSnapEnd () {
-    board.position(game.fen())
-    }
-
-    function updateStatus () {
-        stockfish.postMessage(`position fen ${game.fen()}`);
-        stockfish.postMessage("go depth 10");
-        stockfish.onmessage = (e)=>{
-            if ((e.data).startsWith("info depth 10 seldepth") && (e.data).includes(`multipv ${LINE_NUMBER}`)){
-                const index=(e.data).indexOf(" pv ");
-                const line = e.data.substring(index+4);
-                const movesList = line.split(" ");
+    function startTraining(movesList){
+        function engineMove(movesList){
+            if (game.turn()!=data.rows[0].turn){
+                game.move({
+                    from: movesList[0].substring(0,2),
+                    to:movesList[0].substring(2)
+                });
+                board.position(game.fen());
+                movesList.shift();
                 
-                if (game.turn()!=data.rows[0].turn){
-                    game.move({
-                        from: movesList[0].substring(0,2),
-                        to:movesList[0].substring(2)
-                    });
-                    board.position(game.fen());
-                }
             }
-
+    
         }
-        var status = ''
-
-        var moveColor = 'White'
-        if (game.turn() === 'b') {
-            moveColor = 'Black'
-        }
-
-        // checkmate?
-        if (game.in_checkmate()) {
-            status = 'Game over, ' + moveColor + ' is in checkmate.'
-        }
-
-        // draw?
-        else if (game.in_draw()) {
-            status = 'Game over, drawn position'
-        }
-
-        // game still on
-        else {
-            status = moveColor + ' to move'
-
-            // check?
-            if (game.in_check()) {
-            status += ', ' + moveColor + ' is in check'
+    
+        function isBestMove(movesList,source,target){
+            if (movesList[0]===source+target){
+                movesList.shift()
+                return true;
+            } else {
+                return false;
             }
         }
 
-        $status.html(status)
-        $fen.html(game.fen())
-        $pgn.html(game.pgn())
-        }
 
+        function onDragStart (source, piece, position, orientation) {
+            // do not pick up pieces if the game is over
+            if (game.game_over()) return false
+    
+            // only pick up pieces for the side to move
+            if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+                (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+                return false
+            }
+        }
+    
+        function onDrop (source, target) {
+            // see if the move is legal
+            if (isBestMove(movesList,source,target)){
+                var move = game.move({
+                    from: source,
+                    to: target,
+                    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+                })
+
+                engineMove(movesList)
+
+
+            }
+    
+            return 'snapback';
+            
+        }
+        
+    
+        // update the board position after the piece snap
+        // for castling, en passant, pawn promotion
+        function onSnapEnd () {
+            board.position(game.fen())
+        }
+    
         var config = {
         draggable: true,
         position: game.fen(),
         onDragStart: onDragStart,
         onDrop: onDrop,
         onSnapEnd: onSnapEnd
-    }
-    board = Chessboard('myBoard', config)
+        }
+        board = Chessboard('myBoard', config)
+    
+        
+        engineMove(movesList)
 
-    updateStatus()
+    }
+
+
     
 }
 
